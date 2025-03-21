5012ae60f59b35e3a7d33a6f2f8240199738b4f4
Jeyaruban jenushan <jjenushan550@gmail.com>
2025-03-21 17:45:37 +0530

    backendApplicationsetup

---
+++ HospitalManagementApplicationBackend/src/main/java/org/ai/hospitalmanagementapplicationbackend/HospitalManagementApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ai.hospitalmanagementapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementApplicationBackendApplication.class, args);
+    }
+
+}
---
+++ HospitalManagementApplicationBackend/src/test/java/org/ai/hospitalmanagementapplicationbackend/HospitalManagementApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ai.hospitalmanagementapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
